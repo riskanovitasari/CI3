2ad766e09bafcd662d5c0341a0d990d5169e4aa7
riskanovitasari <riska502@gmail.com>
2018-03-26 13:23:28 +0700

    penambahana data

---
+++ application/views/home.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+ 	 <title>Raja Ampat</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/font-awesome.css" type="text/css">  
+
+    </head>
+  <body>
+ 
+  <nav class="navbar navbar-fixed-top">
+
+	<a class= "navbar-brand" href="#">WEB RISKA</a> 
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>                     
+				</button>
+			</div>   
+			<div class="single-page-nav sticky-wrapper" id="tmNavbar">
+				<ul class="nav navbar-nav">
+					<li><a class="page-scroll" href="#sejarah">Home</a></li>
+					<li><a class="page-scroll" href="#testimornial-area">About</a></li>
+					
+					
+				</ul>
+			</div>   
+		</div>
+	</nav>   				
+</div>
+	<div class="row">
+		<section id="sejarah">
+			<div class="col-md-12">
+				<h1>SEJARAH</h2>
+				<font size="4px">
+				<p>
+				  Kepulauan Raja Ampat merupakan destinasi wisata yang terletak di wilayah Papua. Wisata ini sudah dikenal oleh seluruh dunia karena keelokan alamnya yang sangat mempesona. Salah satu bentuk kepopuleran objek wisata Raja Ampat adalah dengan adanya film dokumenter yang dibuat oleh Avant Premiere yang berjudul " Edis Paradise 3 ", dimana dalam film tersebut menceritakan keindahan alam bawah laut Raja Ampat yang berada di kawasan Papua, yang mana wisata ini juga dijuluki sebagi kawasan Amazon Lautan Dunia. Julukan tersebut diberikan karena letak dari tempat wisata ini yang berada dipusat segitiga karang dunia. Wisata Kepulauan Raja Ampat berada dikawasan teritorial Papua Barat, yang merupakan sebuah gugusan pulau yang tersebar dengan jumlahnya berkisar 610 pulau, akan tetapi hanya ada 35 pulau yang dihuni oleh penduduk. 
+				 Asal mula nama Raja Ampat menurut mitos masyarakat setempat berasal dari seorang wanita yang menemukan tujuh telur. Empat butir di antaranya menetas menjadi empat orang pangeran yang berpisah dan masing-masing menjadi raja yang berkuasa di Waigeo, Salawati, Misool Timur dan Misool Barat. Sementara itu, tiga butir telur lainnya menjadi hantu, seorang wanita, dan sebuah batu.
+
+				Dalam perjalanan sejarah, wilayah Raja Ampat telah lama dihuni oleh masyarakat bangsawan dan menerapkan sistem adat Maluku. Dalam sistem ini, masyarakat sekumpulan manusia. Tiap desa dipimpin oleh seorang raja. Semenjak berdirinya lima kesultanan muslim di Maluku, Raja Ampat menjadi bagian klaim dari Kesultanan Tidore. Setelah Kesultanan Tidore takluk dari Belanda, Kepulauan Raja Ampat menjadi bagian klaim Hindia Belanda.
+				</p>
+				</font>
+			</div>
+		</section>
+	</div>
+<div class="row">
+	<section id="testimornial-area">
+						<div class="container">
+							<div class="row text-center">
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img src="assets/img/images1baru.jpg" alt="Image">
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img src="assets/img/images2baru.jpg" alt="Image">
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img src="assets/img/images3baru.jpg" alt="Image">
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img src="assets/img/images4baru.jpg" alt="Image">
+									</div>
+									<p id="dets"></p>
+								</div>
+							</div>
+							<br>
+							<div class="row text-center">
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img src="assets/img/imagesbaru5.jpg" alt="Image">
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img src="assets/img/imagesbaru6.jpg" alt="Image">
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img src="assets/img/imagesgambar7.jpg" alt="Image">
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xxs-12">
+									<div class="testimonial-content">
+										<img src="assets/img/imagesbaru8.jpg" alt="Image">
+									</div>
+									<p id="dets"></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+<div class="row">
+	
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+  </body>
+</html>
